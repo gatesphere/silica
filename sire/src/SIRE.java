d7fcb11ccffea841ca56ae025c08f8f2c3bc8d8d
Jacob Peck <suschord@suspended-chord.info>
2012-03-04 22:29:42 -0500

    Worked on sire a bit.

--- sire/src/SIRE.java
+++ sire/src/SIRE.java
@@ -8,22 +8,51 @@ import org.jfugue.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SIRE extends Frame {
+public class SIRE extends Frame implements ComponentListener, ActionListener {
+  // Graphics (processing) area
   public SIREPApplet sire_app;
+  
+  // JFugue (MIDI) stuff
+  public Player player;
+  public Pattern pattern = new Pattern("");
+  
+  public Button pause_play;
+  public Button stop;
   
   public SIRE() {
     super("sire - the silica rendering engine");
     
+    addComponentListener(this);
+    
     setLayout(new BorderLayout());
+    
     sire_app = new SIREPApplet();
+    
+    Panel control_panel = new Panel();
+    pause_play = new Button("Pause");
+    pause_play.setEnabled(false);
+    pause_play.setActionCommand("Pause");
+    stop = new Button("Stop");
+    stop.setEnabled(false);
+    stop.setActionCommand("Stop");
+    
+    control_panel.add(pause_play);
+    control_panel.add(stop);
+    
+    
     add(sire_app, BorderLayout.CENTER);
+    add(control_panel, BorderLayout.SOUTH);
     setResizable(true);
     setSize(500,500);
+    
     addWindowListener(new WindowAdapter(){
       public void windowClosing(WindowEvent we){
+        ((SIRE)we.getWindow()).player.close();
         System.exit(0);
       }
     });    
+    
+    player = new Player();
     
     sire_app.init();
   }
@@ -34,6 +63,7 @@ public class SIRE extends Frame {
   
   public static void main(String[] args) {
     SIRE sire = new SIRE();
+    sire.setLocationRelativeTo(null);
     sire.setVisible(true);
     
     
@@ -45,4 +75,29 @@ public class SIRE extends Frame {
     //Player player = new Player();
     //player.play(new Pattern("C D E D C"));
   }
+  
+  
+  
+  // action listener
+  public void actionPerformed(ActionEvent ae) {
+    String s = ae.getActionCommand(); 
+    if (s.equals("Pause")) { 
+      this.player.pause();
+    } 
+    else if (s.equals("Resume")) { 
+      this.player.resume(); 
+    } 
+    else if (s.equals("Stop")) { 
+      this.player.stop();
+    }
+  } 
+  
+  
+  // component listener
+  public void componentHidden(ComponentEvent e) {}
+  public void componentMoved(ComponentEvent e) {}
+  public void componentResized(ComponentEvent e) {
+    ((SIRE)e.getComponent()).sendUpdateEvent();
+  }
+  public void componentShown(ComponentEvent e) {}
 }
--- sire/src/SIREPApplet.java
+++ sire/src/SIREPApplet.java
@@ -8,16 +8,19 @@ import org.jfugue.*;
   
 public class SIREPApplet extends PApplet {
   public void setup() {
-    
     PFont deja_vu_serif_16 = loadFont("DejaVuSerif-16.vlw");
     textFont(deja_vu_serif_16);
-    fill(0, 102, 153);
+    noStroke();
     redraw();
     noLoop();
   }
   
   public void draw() {
-    text("Welcome to sire.", 100, 100);
+    fill(color(255));
+    rect(0, 0, width, height);
+    fill(0, 102, 153);
+    textAlign(CENTER, CENTER);
+    text("Welcome to sire.", width/2, height/2);
   }
   
   public void sendUpdateEvent() {
