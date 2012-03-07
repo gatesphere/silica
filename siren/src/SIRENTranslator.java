70f5c0961c8aaf89365c44ddfca5ee674fcc820c
Jacob Peck <suschord@suspended-chord.info>
2012-03-06 20:03:53 -0500

    Siren now supports up to 240 concurrent layers.

--- siren/src/SIRENTranslator.java
+++ siren/src/SIRENTranslator.java
@@ -22,7 +22,8 @@ public class SIRENTranslator {
     while(sc.hasNext()) {
       String token = sc.next();
       if(token.startsWith("X") || token.startsWith("V") || 
-         token.startsWith("T") || token.startsWith("I")) {
+         token.startsWith("T") || token.startsWith("I") ||
+         token.startsWith("L")) {
         continue;
       } else { // note
         float length = Float.parseFloat(token.substring(token.indexOf("/") + 1));
@@ -33,17 +34,20 @@ public class SIRENTranslator {
   }
   
   public String getMusicString(String siren_string) {
-    String[] voices = new String[16];
+    String[][] voices = new String[16][16];
     for(int i = 0; i < 16; i++) {
-      voices[i] = "V" + i + " R/0.25";
+      for(int j = 0; j < 16; j++) {
+        voices[i][j] = "V" + i + " L" + j + " R/0.25";
+      }
     }
     
     Scanner sc = new Scanner(siren_string);
     int currentRegister = 5;
     int currentVoice = 0;
+    int currentLayer = 0;
     float currentTime = 0.25f;
     boolean concurrent = false;
-    String tempo = "T120"
+    String tempo = "T120";
     
     /* known bugs:
      * doesn't use JFugue's layers
@@ -53,18 +57,18 @@ public class SIRENTranslator {
       String token = sc.next();
       if(token.startsWith("!")) {
         // tempo
-        String tempo = token.substring(1);
-        tempo = "T" + tempo;
+        String temp = token.substring(1);
+        tempo = "T" + temp;
         continue;
       } else if(token.startsWith("@")) {
         // instrument
         String inst = token.substring(1);
-        voices[currentVoice] = voices[currentVoice] + " I[" + inst + "]";
+        voices[currentVoice][currentLayer] = voices[currentVoice][currentLayer] + " I[" + inst + "]";
         continue;
       } else if(token.startsWith("$")) {
         // volume
         String vol = token.substring(1);
-        voices[currentVoice] = voices[currentVoice] + " X[Volume]=" + vol;
+        voices[currentVoice][currentLayer] = voices[currentVoice][currentLayer] + " X[Volume]=" + vol;
         continue;
       } else if(token.startsWith("{") || token.startsWith("}")) {
         // command grouping
@@ -74,19 +78,24 @@ public class SIRENTranslator {
         continue;
       } else if(token.startsWith("||")) {
         // push voice
-        currentVoice++;
-        if(currentVoice == 9) currentVoice = 10;
-        if(currentVoice > 15) currentVoice = 15;
-        float length = getLengthOfVoice(voices[currentVoice]);
+        currentLayer++;
+        if(currentLayer == 16) {
+          currentLayer = 0;
+          currentVoice++;
+          if(currentVoice == 9) currentVoice = 10;
+          if(currentVoice > 15) currentVoice = 15;
+        }
+        float length = getLengthOfVoice(voices[currentVoice][currentLayer]);
         if(length < currentTime) {
           float delta = currentTime - length;
-          System.out.println("voice: " + currentVoice + " delta: " + delta);
-          voices[currentVoice] = voices[currentVoice] + " R/" + delta;
+          //System.out.println("voice: " + currentVoice + " delta: " + delta);
+          voices[currentVoice][currentLayer] = voices[currentVoice][currentLayer] + " R/" + delta;
         }
         continue;
       } else if(token.startsWith("]")) {
         // pop voice
         currentVoice = 0;
+        currentLayer = 0;
         concurrent = false;
         continue;
       } else {
@@ -97,15 +106,17 @@ public class SIRENTranslator {
         token = token.replace("Y", "G#");
         token = token.replace("Z", "A#");
         
-        voices[currentVoice] = voices[currentVoice] + " " + token;
-        if(currentVoice == 0 && !concurrent) currentTime = getLengthOfVoice(voices[currentVoice]);
+        voices[currentVoice][currentLayer] = voices[currentVoice][currentLayer] + " " + token;
+        if(currentVoice == 0 && !concurrent) currentTime = getLengthOfVoice(voices[currentVoice][currentLayer]);
         continue;
       }
     }
     
-    String music_string = tempo;
+    String music_string = tempo + "\n";
     for(int i = 0; i < 16; i++) {
-      music_string = music_string + " " + voices[i] + " R/0.25\n";
+      for(int j = 0; j < 16; j++) {
+        music_string = music_string + " " + voices[i][j] + " R/0.25\n";
+      }
     }
     System.out.println(music_string);
     return music_string.trim();
