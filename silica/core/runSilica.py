caaac1e82d8692d76e78ffcb545350c8a4baf2c7
Jacob Peck <peckj@upstate.edu>
2013-06-04 13:43:08 -0400

    Further refinements to the Leo workbook.

--- silica/core/runSilica.py
+++ silica/core/runSilica.py
@@ -1,9 +1,11 @@
 #@+leo-ver=5-thin
 #@+node:peckj.20130604112211.2150: * @file core/runSilica.py
 #@@language python
-
+#@+others
+#@+node:peckj.20130604112211.4471: ** run
 # run silica
 def run():
   # dummy code, for now
   print "Hello, world, from silica."
+#@-others
 #@-leo
