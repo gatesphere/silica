6222a8012b39fca61f71d97f44d1d139faa8c268
Jacob Peck <peckj@upstate.edu>
2014-01-14 16:01:08 -0500

    A tiny bit of machinery to allow macros with arguments

--- silica/core/macro.py
+++ silica/core/macro.py
@@ -13,13 +13,18 @@ from silica.core.entity import Entity
 class Macro(Entity):
   #@+others
   #@+node:peckj.20140106180202.4623: *3* __init__
-  def __init__(self, name, value):
+  def __init__(self, name, value, args=None):
     desc = "Macro %s" % name
     super(self.__class__,self).__init__(name, desc)
     self.value = value
+    self.args = args
   #@+node:peckj.20140106180202.4626: *3* expand
-  def expand(self):
-    return self.value
+  def expand(self, callargs=None):
+    out = self.value
+    if callargs is not None:
+      for arg,callarg in zip(self.args,callargs):
+        out = out.replace(arg,callarg)
+    return out
   #@+node:peckj.20140106180202.4625: *3* __str__
   def __str__(self):
     return "< MACRO " + self.name + " >"
